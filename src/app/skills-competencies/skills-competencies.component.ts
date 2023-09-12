17da54b34c3441b3ea45b579fca801f4b7c1ccd6
@ailtonsky <ailtonseiya10@gmail.com>
2023-09-12 12:37:19 -0600

    create component and url api #12

--- src/app/skills-competencies/skills-competencies.component.ts
+++ src/app/skills-competencies/skills-competencies.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillsCompetenciesService } from '../services/skills-competencies.service';
 
 @Component({
   selector: 'app-skills-competencies',
   templateUrl: './skills-competencies.component.html',
   styleUrls: ['./skills-competencies.component.css']
 })
-export class SkillsCompetenciesComponent {
+export class SkillsCompetenciesComponent implements OnInit {
+  constructor(private skillsCompetencies: SkillsCompetenciesService){
+  }
 
+  data: any[] = [];
+
+  ngOnInit(): void {
+    this.skillsCompetencies.getSkillsCompetencies()
+    .subscribe( (data: any) => {
+      console.log(data);
+      //alert(JSON.stringify(data));
+      this.data = data.work_experience;
+    })
+  }
 }
